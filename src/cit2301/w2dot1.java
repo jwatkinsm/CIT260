be28b0ef399a03fe1884eb57b26f6b6528326353
jwatkinsm <wat18023@byui.edu>
2020-04-28 10:48:15 -0400

    week2 assignment 1 finish

--- src/cit2301/w2dot1.java
+++ src/cit2301/w2dot1.java
@@ -1,7 +1,17 @@
 package cit2301;
 
+import java.util.Scanner;
+
 public class w2dot1 {
     public static void main(String[] args){
-        System.out.println("Enter degree in celcius:");
+        Scanner input= new Scanner(System.in);
+       double celcius;
+       double fahrenhiet;
+       System.out.println("This program converts a temperature in degrees from celcius into fahrenhiet.");
+        System.out.print("Enter degree in celcius:");
+        celcius= input.nextDouble();
+        System.out.println("the celcius is "+ celcius);
+        fahrenhiet= (9.0/5) * celcius + 32;
+        System.out.print(celcius + " clecius is " + fahrenhiet + " fahrenhiet.");
     }
 }
