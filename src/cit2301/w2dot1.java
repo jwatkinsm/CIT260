01c65170e5b2670fe7c639e720c8eac71b9bfd2c
jwatkinsm <wat18023@byui.edu>
2020-04-29 12:56:40 -0400

    printf update

--- src/cit2301/w2dot1.java
+++ src/cit2301/w2dot1.java
@@ -1,19 +1,20 @@
 package cit2301;
 
-import java.text.DecimalFormat;
+
 import java.util.Scanner;
 
 public class w2dot1 {
     public static void main(String[] args){
         Scanner input= new Scanner(System.in);
-        DecimalFormat decfor= new DecimalFormat("0.00");
        double celcius;
        double fahrenhiet;
+       String s1= "celcius is";
+       String s2= "in fahrenheit";
        System.out.println("This program converts a temperature in degrees from celcius into fahrenhiet.");
         System.out.print("Enter degree in celcius:");
         celcius= input.nextDouble();
-        System.out.println("the celcius is "+ (decfor.format(celcius)));
+        System.out.printf("the celcius is "+ "%.2f%n",celcius);
         fahrenhiet= (9.0/5) * celcius + 32;
-        System.out.print((decfor.format(celcius)) + " clecius is " + (decfor.format(fahrenhiet)) + " fahrenhiet.");
+        System.out.printf("%.2f %s %.2f %s",celcius,s1,fahrenhiet,s2);
     }
 }
