29bf0d5cbd23498a1a44c5543f500a574e51c648
jwatkinsm <wat18023@byui.edu>
2020-06-27 12:22:20 -0400

    comments

--- src/cit2301/triangle.java
+++ src/cit2301/triangle.java
@@ -21,14 +21,15 @@ package cit2301;
  *          +toString(): String
  */
 public class triangle extends GeometricObject {
+    //variables
     private double side1 = 1.0;
     private double side2 = 1.0;
     private double side3 = 1.0;
-
+    //default
     public triangle()
     {
     }
-
+    //object
     public triangle(double Side1, double Side2, double Side3, String color, boolean filled)
     {
         super(color,filled);
@@ -36,45 +37,41 @@ public class triangle extends GeometricObject {
         this.side2 = Side2;
         this.side3 = Side3;
     }
-
+    //getters
     public double getSide1()
     {
         return side1;
     }
-
     public double getSide2()
     {
         return side2;
     }
-
     public double getSide3()
     {
         return side3;
     }
-
+    //setters
     public void setSide1(double side1) {
         this.side1 = side1;
     }
-
     public void setSide2(double side2) {
         this.side2 = side2;
     }
-
     public void setSide3(double side3) {
         this.side3 = side3;
     }
-
+    //getArea override from GeometricObject
     @Override
     public double getArea() {
         double s = (side1 + side2 + side3) / 2;
         return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
     }
-
+    //getpremitter
     public double getPerimeter()
     {
         return (side1+side2+side3);
     }
-
+    //toString override from GeometricObject
     @Override
     public String toString()
     {
--- src/cit2301/w10dot1.java
+++ src/cit2301/w10dot1.java
@@ -5,23 +5,25 @@ import java.util.Scanner;
 
 public class w10dot1 {
     public static void main(String[] args) {
+        //program info
         System.out.println("This program gets input for three triangles from the user.\n" +
                 "It then creates three Triangle objects and displays the\n" +
                 "description of each.");
         Scanner input = new Scanner(System.in);
+        //variable for each object
         String[] Color= new String[3];
         boolean[] filled= new boolean[3];
         double[] Side1 = new double[3];
         double[] Side2 = new double[3];
         double[] Side3 = new double[3];
         char fillOrNot;
-
-
+        //user input for each object
         for( int i =0;i <3;i++) {
             System.out.println("Enter the color of a triangle (e.g. 'red'):");
             Color[i] = input.nextLine();
             System.out.println("Is the triangle filled (y or n):");
             fillOrNot = input.next().charAt(0);
+            //filled char to boolean outputs
             if(fillOrNot == 'y' || fillOrNot =='Y'){
                 filled[i] = true;
             }
@@ -35,18 +37,20 @@ public class w10dot1 {
             Side1[i] = input.nextDouble();
             Side2[i] = input.nextDouble();
             Side3[i] = input.nextDouble();
+            //empty line
             input.nextLine();
         }
-
+//objects
         triangle tri1 = new triangle(Side1[0], Side2[0], Side3[0],Color[0],filled[0]);
         triangle tri2 = new triangle(Side1[1], Side2[1], Side3[1],Color[1],filled[1]);
         triangle tri3 = new triangle(Side1[2], Side2[2], Side3[2],Color[2],filled[2]);
-
+        //arraylist
         ArrayList<GeometricObject> triangles = new ArrayList<GeometricObject>();
+        //add objects
         triangles.add(tri1);
         triangles.add(tri2);
         triangles.add(tri3);
-
+        //to string outputs
         System.out.printf(tri1.toString()+ "Area =" +"%.2f%n%n",tri1.getArea());
         System.out.printf(tri2.toString()+ "Area =" +"%.2f%n%n",tri2.getArea());
         System.out.printf(tri3.toString()+ "Area =" +"%.2f%n%n",tri3.getArea());
